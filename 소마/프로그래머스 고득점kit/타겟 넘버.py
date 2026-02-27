1f99a7b4ebf02ea84947a02bf3b19f91519fa8fe
scorve12 <scorve5322@gmail.com>
2026-02-27 20:09:09 +0900

    feat: 260227 문제 풀이

--- 소마/프로그래머스 고득점kit/타겟 넘버.py
+++ 소마/프로그래머스 고득점kit/타겟 넘버.py
@@ -5,11 +5,3 @@ def solution(numbers, target):
         return 0
     else:
         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
-    
-def solution(numbers, target):
-    if not numbers and target == 0:
-        return 1
-    elif not numbers:
-        return 0
-    
-    return solution(numbers[:1], target-numbers[0]) + solutions(numbers[:1], target+numbers[0])
